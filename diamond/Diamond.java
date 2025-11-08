4ba7f4cd9ca929a5458137074540a8be9c26e0eb
kb1etr <schuett@hartford.edu>
2025-11-08 16:29:08 -0500

    Update Diamond.java

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+  
+  Scanner keyInput = new Scanner(System.in);
+
+  int number;
+
+  System.out.print("Enter a number: ");
+  number = keyInput.nextInt();
+
+  int [][] table = new int[number][number];
+  
+  System.out.println(number);
+  
+
+  
   }
 }
