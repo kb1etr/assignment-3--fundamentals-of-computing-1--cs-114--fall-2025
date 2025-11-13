06f6c75d88b4b80ffb88b82e9ae64394523008c8
kb1etr <schuett@hartford.edu>
2025-11-13 09:02:15 -0500

    Add work

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
 public class Diamond {
-public static void main(String[] args) {
+  public static void main(String[] args) {
+    Scanner keyInput = new Scanner(System.in);
 
-  Scanner keyInput = new Scanner(System.in);
+    int number = 0;
 
- int number = 0;
+    boolean workingOnTopHalf = true;
 
- boolean workingOnTopHalf = true;
+    try {
+      System.out.print("Enter a number: ");
+      number = keyInput.nextInt();
 
- System.out.print("Enter a number: ");
- number = keyInput.nextInt();
+      for (int row = 1; row <= number; row++) {
+        for (int space = number - row; space >= 1; space--) {
+          System.out.print(" |");
+        }
 
-    for (int row = 1; row <= number; row++) {
-      for (int space = number - row; space >= 1; space--) {
-        System.out.print(" |");
+        for (int ast = 1; ast <= row; ast++) {
+          System.out.print("*");
+        }
+
+        System.out.println();
       }
-      for (int ast = 1; ast <= row; ast++) {
-        System.out.print("*");
-      }
+      } catch (Exception e) {
+        System.out.println("Invalid input. Please enter an integer.");
 
-      System.out.println();
-
-
-    
+        return;
     }
-
+    
+    keyInput.close();
   }
 }
