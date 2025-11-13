5c3838d3d218284b524120daab24950a008c0a0c
kb1etr <schuett@hartford.edu>
2025-11-12 19:57:15 -0500

    Update Diamond.java

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,27 +1,39 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
   
   Scanner keyInput = new Scanner(System.in);
 
-  int number;
+  int number = 0;
 
   System.out.print("Enter a number: ");
   number = keyInput.nextInt();
 
   int [][] table = new int[number][number];
   
-  for (int row = 0; row < table.length; row 223++) {
-    for (int col = 0; col < (table[row].length - 1); col++) {
-      System.out.print((row) + ", " + (col) +" : ");
+table [0][1] =1 ;table [1][0] = 1; table [1][1] = 1; table [1][2] = 1; table  [2][1] =1;
+
+
+
+
+  for (int row = 0; row < table.length; row++) {
+    for (int col = 0; col < (table[row].length); col++) {
+      // System.out.print((table[row][col])+" : ");
+      if ((table[row][col])== 1) {
+        System.out.print("*");
+
+      }
+      else {
+        System.out.print(" ");
+      
+      }
+
       } 
     
       System.out.println();
     
-  
     }
 
-  
   }
 }
