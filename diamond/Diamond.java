7c0d93be339a41c9c176766db3087012dafa5c87
kb1etr <schuett@hartford.edu>
2025-11-16 18:25:55 -0500

    Update Diamond.java

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -11,94 +11,88 @@ public class Diamond {
     int spacelength;
 
     try { 
-
       System.out.print("Enter a number: "); 
-
       inNumber = keyInput.nextInt(); 
-
       row = inNumber;
 
-      if (inNumber % 2 == 0) {  // Check if input is odd or even
-
-        System.out.println("even"); 
+      if (inNumber % 2 == 0) {  // Check if input is odd or even 
 
         for (row = 0; row <= inNumber/2; row++){ //for first half of matrix
+         if (row == 0) spacelength = inNumber-1;
+         else spacelength = (inNumber/2 - row)*2; 
+         for (int space = 1; space <= spacelength; space++){
+          System.out.print(" ");
+          }
 
-          if (row == 0) spacelength = inNumber-1;
-
-          else spacelength = (inNumber/2 - row)*2; 
-
-          for (int space = 1; space <= spacelength; space++){
-
-            System.out.print(" ");
-          } 
-          //for (int space = 1; space <=inNumber/2; space++)     
-
-          if (row == 0  || row == inNumber) {  // prints a single asterisk on the first and last row
+          if (row == 0  || row == inNumber) {  // Special case: prints a single asterisk on the first and last row
             System.out.print("* ");
           } 
           else if (row <= inNumber/2){
-
-            for (int ast = 1; ast <= row*2; ast++ ){
-
-              System.out.print("* ");
-
+           for (ast = 1; ast <= row*2; ast++ ){
+            System.out.print("* ");
             }
           }
           System.out.println();
         }
 
-        for (row = inNumber/2 -1; row >= 0; row--){ //for second half of matrix
-
-          if (row == 0) spacelength = inNumber-1;
-
-          else spacelength = (inNumber/2 - row)*2; 
-
-           for (int space = 1; space <= spacelength; space++){
-           System.out.print(" ");
-           } 
-          //for (int space = 1; space <=inNumber/2; space++)     
-
-          if (row == 0  || row == inNumber) {  // prints a single asterisk on the first and last row
+        for (row = inNumber/2 -1; row >= 0; row--){ //for bottom half of the iamond
+         if (row == 0) spacelength = inNumber-1;
+         else spacelength = (inNumber/2 - row)*2; 
+          for (int space = 1; space <= spacelength; space++){
+          System.out.print(" ");
+          }
+          
+          if (row == 0  || row == inNumber) {  // Special case: prints a single asterisk on the first and last row
             System.out.print("* ");
           }
-          else if (row <= inNumber/2){  
+          else if (row <= inNumber/2){
 
-          for (int ast = 1; ast <= row*2; ast++ ){
-
-            System.out.print("* ");
-
-          }
+            for (ast = 1; ast <= row*2; ast++ ){
+             System.out.print("* ");
+            }
           }
           System.out.println();
         }
-
-      } 
-      else { //odd
-    // Update variables for spaces and asterisks
-      System.out.println("odd"); 
-
-    for (row = 1; row <= inNumber; row++) {
-        for (int space = (inNumber / row); space > 1; space--) {
-         System.out.print(" ");
-        }
-        for (int ast = 1; ast <= row; ast++) {
-         System.out.print("*");
-        }
-        System.out.println();
       }
-
-      /*for (int row = 1; row <= inNumber; row++){
-        if (row == 1 || row == inNumber){
+    else {  //for an odd number input
+      for (row = 0; row <= (inNumber-1)/2; row++){ //for first half of matrix
+          if (row == 0) spacelength = (inNumber-1)/2;
+          else spacelength = ((inNumber-1)/2 - row); 
+          for (int space = 1; space <= spacelength; space++){ // Print number of leading spaces
           System.out.print(" ");
+          }
+          
+          if (row == 0  || row == inNumber) {  // prints a single asterisk on the first and last row
+            System.out.print("*");
+          } 
+          else if (row <= inNumber/2){
+            for (ast = 1; ast <= row*2+1; ast++ ){
+            
+              System.out.print("*");
+            }
+          }          
+          System.out.println();       
         }
-        
-        for (ast = 1; ast <= inNumber-row; ast++)
-        System.out.print("*");
-        
-        System.out.println();
-      }*/
 
+      for (row = ((inNumber-1)/2) -1; row >= 0; row--){ //for first half of matrix
+          if (row == 0) spacelength = (inNumber-1)/2;
+          else spacelength = ((inNumber-1)/2 - row); 
+          for (int space = 1; space <= spacelength; space++){ // Print number of leading spaces
+          System.out.print(" ");
+          }               
+          
+          if (row == 0  || row == inNumber) {  // special case: prints a single asterisk on the first and last row
+            System.out.print("*");
+          } 
+          else if (row <= inNumber){                                                                                                   
+            for (ast = 1; ast <= row*2+1; ast++ ){
+            
+              System.out.print("*");
+              
+            }
+          }          
+          System.out.println();       
+        }
     }
     
   } catch (Exception e) {
