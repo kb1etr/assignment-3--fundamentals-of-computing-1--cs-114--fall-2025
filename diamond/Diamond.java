41f694a1a6ce7c5a19a5d031fc6ba4fcbd6cda87
kb1etr <schuett@hartford.edu>
2025-11-17 20:00:41 -0500

    Update Diamond.java

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -15,14 +15,14 @@ public class Diamond {
 
       if (inNumber % 2 == 0) {  // Check if input is odd or even - EVEN CASE
 
-        for (row = 0; row <= inNumber/2; row++){ //for first half of matrix
+        for (row = 0; row <= inNumber/2; row++){ //for first half the diamond
           if (row == 0) spaceLength = inNumber-1;
           else spaceLength = (inNumber/2 - row)*2; 
           for (int space = 1; space <= spaceLength; space++){
           System.out.print(" ");
           }
 
-          if (row == 0) {  // Special case: prints a single asterisk on the first and last row
+          if (row == 0) {  // Special case: prints a single asterisk on the first row
             System.out.print("* ");
           } 
           else if (row <= inNumber/2){
@@ -42,7 +42,7 @@ public class Diamond {
           System.out.print(" ");
           }
 
-          if (row == 0) {  // Special case: prints a single asterisk on the first and last row
+          if (row == 0) {  // Special case: prints a single asterisk last row
             System.out.print("* ");
           } 
           else if (row <= inNumber/2){
@@ -56,14 +56,14 @@ public class Diamond {
       }
 
     else {  //for an odd number input - ODD CASE
-      for (row = 0; row <= (inNumber-1)/2; row++){ //for first half of matrix
+      for (row = 0; row <= (inNumber-1)/2; row++){ //for first half of the diamond
           if (row == 0) spaceLength = (inNumber-1)/2;
           else spaceLength = ((inNumber-1)/2 - row); 
           for (int space = 1; space <= spaceLength; space++){ // Print number of leading spaces
           System.out.print(" ");
           }
 
-          if (row == 0) {  // prints a single asterisk on the first and last row
+          if (row == 0) {  // prints a single asterisk on the first row
            System.out.print("*");
           }
           else if (row <= inNumber/2){
@@ -75,14 +75,14 @@ public class Diamond {
           System.out.println();
         }
 
-      for (row = ((inNumber-1)/2) -1; row >= 0; row--){ //for first half of matrix
+      for (row = ((inNumber-1)/2) -1; row >= 0; row--){ //for bottom half of the diamond
           if (row == 0) spaceLength = (inNumber-1)/2;
           else spaceLength = ((inNumber-1)/2 - row); 
           for (int space = 1; space <= spaceLength; space++){ // Print number of leading spaces
           System.out.print(" ");
           }
 
-          if (row == 0) {  // special case: prints a single asterisk on the first and last row
+          if (row == 0) {  // special case: prints a single asterisk on the last row
             System.out.print("*");
           } 
           else if (row <= inNumber){
