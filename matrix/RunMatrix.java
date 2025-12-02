87713baaf458ad3da1a30d7a5e93a9a676102c66
kb1etr <schuett@hartford.edu>
2025-12-02 09:20:52 -0500

    Update RunMatrix.java

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -34,7 +34,7 @@ public class RunMatrix {
         System.out.println("Printing flipped matrix:");
         myMatrix.printMatrix();
 
-        scanner.close(); // Close the scanner to release resources
+        scanner.close();
     
   }
 }
